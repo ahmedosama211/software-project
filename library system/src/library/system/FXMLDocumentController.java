ce1be4981809948d57784b42e47967c4f7b154ec
ahmedosama211 <33431885+ahmedosama211@users.noreply.github.com>
2020-03-13 23:59:43 +0200

    Add files via upload

---
+++ library system/src/library/system/FXMLDocumentController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library.system;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author ahmed
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+private AnchorPane login_pane;   
+    @FXML
+    private ToggleButton login_toggle;
+    
+    @FXML 
+     
+     
+     
+     
+     
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
